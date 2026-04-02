70cee9d4b688b605a8e41817a5f1395f43bbd6e6
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-04-03 03:40:29 +0400

    feat(templates): add main page with submission form

---
+++ templates/index.php
@@ -0,0 +1,10 @@
+<h1 class="display-3">Анализатор страниц</h1>
+<p class="lead">
+    Бесплатно проверяйте сайты на SEO-пригодность
+</p>
+<form action="/urls" method="POST">
+    <div class="d-flex">
+        <input class="form-control me-2" type="url" name="url" placeholder="https://www.example.com" aria-label="url" />
+        <button type="submit" class="btn btn-primary">Проверить</button>
+    </div>
+</form>
