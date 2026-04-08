79c6fd162930b2c98380fc5c992141b905e84597
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-04-08 05:49:13 +0400

    feat(app): add url input validation

--- public/index.php
+++ public/index.php
@@ -3,9 +3,11 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use DI\ContainerBuilder;
+use Slim\Routing\RouteContext;
 use Slim\Factory\AppFactory;
 use Slim\Views\PhpRenderer;
 use Slim\Flash\Messages;
+use Valitron\Validator;
 use Hexlet\Code\Database;
 use Hexlet\Code\UrlRepository;
 
@@ -39,7 +41,7 @@ $app->get('/', function (Request $request, Response $response, $args) use ($rend
     $flash = $this->get(Messages::class);
     return $renderer->render($response, 'index.php', [
         'title' => 'Анализатор страниц',
-        'flash' => $flash
+        'flash' => $flash->getMessages()
     ]);
 });
 
@@ -49,21 +51,35 @@ $app->get('/urls', function (Request $request, Response $response, $args) use ($
     return $renderer->render($response, 'urls.php', [
         'title' => 'Анализатор страниц - Сайты',
         'urls' => $urls,
-        'flash' => $flash
+        'flash' => $flash->getMessages()
     ]);
 });
 
-$app->post('/urls', function (Request $request, Response $response, $args) use ($repo) {
-    $routeParser = $this->getRouteCollector()->getRouteParser();
+$app->post('/urls', function (Request $request, Response $response, $args) use ($renderer, $repo) {
+    $routeParser = RouteContext::fromRequest($request)->getRouteParser();
     $flash = $this->get(Messages::class);
     $data = $request->getParsedBody();
 
-    $url = trim($data['url'] ?? '');
+    $validator = new Validator($data);
+    $validator->rule('required', 'url')->message('URL не должен быть пустым');
+    $validator->rule('lengthMax', 'url', 255)->message('URL превышает 255 символов');
+    $validator->rule('url', 'url')->message('Некорректный URL');
+
+    if (!$validator->validate()) {
+        $validatorErrors = array_merge(...array_values($validator->errors()));
+        return $renderer->render($response, 'index.php', [
+            'title' => 'Анализатор страниц',
+            'urlValue' => $data['url'],
+            'flash' => ['danger' => $validatorErrors]
+        ])->withStatus(422);
+    }
+
+    $url = trim($data['url']);
     $parsedUrl = parse_url($url);
     $normalizedUrl = sprintf("%s://%s", $parsedUrl['scheme'], $parsedUrl['host']);
 
     if ($existing = $repo->getByName($normalizedUrl)) {
-        $flash->addMessage("warning", "Страница уже существует");
+        $flash->addMessage('warning', 'Страница уже существует');
         return $response->withStatus(302)->withHeader(
             'Location',
             $routeParser->urlFor('url', ['id' => $existing['id']])
@@ -71,7 +87,7 @@ $app->post('/urls', function (Request $request, Response $response, $args) use (
     }
 
     $id = $repo->insert($normalizedUrl);
-    $flash->addMessage("success", "Страница успешно добавлена");
+    $flash->addMessage('success', 'Страница успешно добавлена');
     return $response->withStatus(302)->withHeader('Location', $routeParser->urlFor('url', ['id' => $id]));
 });
 
@@ -81,7 +97,7 @@ $app->get('/urls/{id}', function (Request $request, Response $response, $args) u
     return $renderer->render($response, 'url.php', [
         'title' => 'Анализатор страниц - Детали',
         'url' => $url,
-        'flash' => $flash
+        'flash' => $flash->getMessages()
     ]);
 })->setName('url');
 
