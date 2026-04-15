743394edac5580017d643be251820f62b6625bb2
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-04-15 21:30:56 +0400

    feat(templates): add http checks table to url detail page

--- templates/url.php
+++ templates/url.php
@@ -15,3 +15,32 @@
     </tr>
   </tbody>
 </table>
+<form method="post" action="/urls/<?= $url['id'] ?>/checks">
+  <input type="submit" value="Запустить проверку">
+</form>
+<table data-test="checks">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Код ответа</th>
+      <th>h1</th>
+      <th>title</th>
+      <th>description</th>
+      <th>Дата создания</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($checks as $check): ?>
+    <tr>
+      <td><?= $check['id'] ?></td>
+      <td><?= $check['status_code'] ?></td>
+      <td><?= $check['h1'] ?></td>
+      <td><?= $check['title'] ?></td>
+      <td><?= $check['description'] ?></td>
+      <td><?= $check['created_at'] ?></td>
+    </tr>
+    <?php endforeach ?>
+  </tbody>
+</table>
+
+
