7531336b00e1f884ba3810cf085f40f7be51fe9a
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-04-15 21:26:00 +0400

    feat(db): add url_checks table

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ create table if not exists urls (
     name varchar(255),
     created_at timestamp
 );
+
+create table if not exists url_checks (
+    id bigint primary key generated always as identity,
+    url_id bigint references urls(id) not null,
+    status_code int,
+    h1 varchar(255),
+    title text,
+    description text,
+    created_at timestamp
+);
