5db6527e24e88072218121e29e84b06ba7b40bd7
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-04-18 00:29:02 +0400

    feat(app): implement url checking functionality

--- public/index.php
+++ public/index.php
@@ -8,8 +8,11 @@ use Slim\Factory\AppFactory;
 use Slim\Views\PhpRenderer;
 use Slim\Flash\Messages;
 use Valitron\Validator;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 use Hexlet\Code\Database;
 use Hexlet\Code\UrlRepository;
+use Hexlet\Code\CheckRepository;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -17,7 +20,8 @@ session_start();
 
 // Database
 $pdo = Database::connect(getenv('DATABASE_URL'));
-$repo = new UrlRepository($pdo);
+$urlRepo = new UrlRepository($pdo);
+$checkRepo = new CheckRepository($pdo);
 
 // Container
 $containerBuilder = new ContainerBuilder();
@@ -36,6 +40,9 @@ $app->addErrorMiddleware(true, true, true);
 $renderer = new PhpRenderer(__DIR__ . '/../templates');
 $renderer->setLayout('layout.php');
 
+// Guzzle
+$client = new Client();
+
 // Routes
 $app->get('/', function (Request $request, Response $response, $args) use ($renderer) {
     $flash = $this->get(Messages::class);
@@ -45,9 +52,9 @@ $app->get('/', function (Request $request, Response $response, $args) use ($rend
     ]);
 });
 
-$app->get('/urls', function (Request $request, Response $response, $args) use ($renderer, $repo) {
+$app->get('/urls', function (Request $request, Response $response, $args) use ($renderer, $urlRepo) {
     $flash = $this->get(Messages::class);
-    $urls = $repo->getAll();
+    $urls = $urlRepo->getAll();
     return $renderer->render($response, 'urls.php', [
         'title' => 'Анализатор страниц - Сайты',
         'urls' => $urls,
@@ -55,7 +62,7 @@ $app->get('/urls', function (Request $request, Response $response, $args) use ($
     ]);
 });
 
-$app->post('/urls', function (Request $request, Response $response, $args) use ($renderer, $repo) {
+$app->post('/urls', function (Request $request, Response $response, $args) use ($renderer, $urlRepo) {
     $routeParser = RouteContext::fromRequest($request)->getRouteParser();
     $flash = $this->get(Messages::class);
     $data = $request->getParsedBody();
@@ -78,27 +85,76 @@ $app->post('/urls', function (Request $request, Response $response, $args) use (
     $parsedUrl = parse_url($url);
     $normalizedUrl = sprintf("%s://%s", $parsedUrl['scheme'], $parsedUrl['host']);
 
-    if ($existing = $repo->getByName($normalizedUrl)) {
+    if ($existing = $urlRepo->getByName($normalizedUrl)) {
         $flash->addMessage('warning', 'Страница уже существует');
         return $response->withStatus(302)->withHeader(
             'Location',
-            $routeParser->urlFor('url', ['id' => $existing['id']])
+            $routeParser->urlFor('url', ['url_id' => $existing['id']])
         );
     }
 
-    $id = $repo->insert($normalizedUrl);
+    $id = $urlRepo->insert($normalizedUrl);
     $flash->addMessage('success', 'Страница успешно добавлена');
-    return $response->withStatus(302)->withHeader('Location', $routeParser->urlFor('url', ['id' => $id]));
+    return $response->withStatus(302)->withHeader('Location', $routeParser->urlFor('url', ['url_id' => $id]));
 });
 
-$app->get('/urls/{id}', function (Request $request, Response $response, $args) use ($renderer, $repo) {
+$app->get('/urls/{url_id}', function (
+    Request $request,
+    Response $response,
+    array $args
+) use (
+    $renderer,
+    $urlRepo,
+    $checkRepo
+) {
     $flash = $this->get(Messages::class);
-    $url = $repo->getById($args['id']);
+    $url = $urlRepo->getById($args['url_id']);
+    $checks = $checkRepo->getByUrlId($args['url_id']);
     return $renderer->render($response, 'url.php', [
         'title' => 'Анализатор страниц - Детали',
         'url' => $url,
+        'checks' => $checks,
         'flash' => $flash->getMessages()
     ]);
 })->setName('url');
+
+$app->post('/urls/{url_id}/checks', function (
+    Request $request,
+    Response $response,
+    $args
+) use (
+    $client,
+    $urlRepo,
+    $checkRepo
+) {
+    $routeParser = RouteContext::fromRequest($request)->getRouteParser();
+    $flash = $this->get(Messages::class);
+
+    $url = $urlRepo->getById($args['url_id']);
+    try {
+        $check = $client->get($url['name']);
+        $status = $check->getStatusCode();
+        $html = $check->getBody()->getContents();
+    } catch (\Throwable $th) {
+        $flash->addMessage('danger', 'Сайт недоступен');
+        return $response->withStatus(302)->withHeader(
+            'Location',
+            $routeParser->urlFor('url', ['url_id' => $args['url_id']])
+        );
+    }
+
+    $crawler = new Crawler($html);
+    $h1 = ($node = $crawler->filter('h1'))->count() ? $node->text() : null;
+    $title = ($node = $crawler->filter('title'))->count() ? $node->text() : null;
+    $description = ($node = $crawler->filter('meta[name="description"]'))->count() ? $node->attr('content') : null;
+
+    $checkRepo->insert($args['url_id'], $status, $h1, $title, $description);
+
+    $flash->addMessage('success', 'Страница успешно проверена');
+    return $response->withStatus(302)->withHeader(
+        'Location',
+        $routeParser->urlFor('url', ['url_id' => $args['url_id']])
+    );
+});
 
 $app->run();
