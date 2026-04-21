25b4c6e7725a556d415627eb5c95088e527c5272
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-04-22 03:14:39 +0400

    feat(repo): include last check timestamp and status when fetching all entries from urls

--- src/UrlRepository.php
+++ src/UrlRepository.php
@@ -27,23 +27,38 @@ class UrlRepository
 
     public function getAll(): array
     {
-        $stmt = $this->pdo->prepare("SELECT * FROM urls ORDER BY created_at DESC");
+        $stmt = $this->pdo->prepare("
+            SELECT
+                urls.id,
+                urls.name,
+                urls.created_at,
+                url_checks.status_code,
+                url_checks.created_at as last_checked_at
+            FROM urls
+            LEFT JOIN url_checks ON urls.id = url_checks.url_id
+                AND url_checks.id = (
+                    SELECT MAX(id)
+                    FROM url_checks
+                    WHERE url_id = urls.id
+                )
+            ORDER BY last_checked_at DESC NULLS LAST
+        ");
         $stmt->execute();
-        $data = $stmt->fetchAll();
-        if ($data === false) {
+        $result = $stmt->fetchAll();
+        if ($result === false) {
             throw new \Exception("Failed to fetch");
         }
-        return $data;
+        return $result;
     }
 
     public function insert(string $name): int
     {
         $stmt = $this->pdo->prepare("INSERT INTO urls (name, created_at) VALUES (:name, NOW()) RETURNING id");
         $stmt->execute(['name' => $name]);
-        $data = $stmt->fetchColumn();
-        if ($data === false) {
+        $result = $stmt->fetchColumn();
+        if ($result === false) {
             throw new \Exception("Failed to fetch");
         }
-        return $data;
+        return $result;
     }
 }
