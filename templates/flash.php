4a2b1e4c799ed01847ea7bcaea78244b4526233a
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-04-25 17:57:07 +0400

    style(templates): fix code style issues

--- templates/flash.php
+++ templates/flash.php
@@ -1,5 +1,5 @@
 <?php if (isset($flash)) : ?>
-    <?php foreach ($flash as $type => $messages):  ?>
+    <?php foreach ($flash as $type => $messages) :  ?>
         <?php foreach ($messages as $message) : ?>
             <div class="container alert alert-<?= $type ?: 'warning' ?>" role="alert">
                 <?= htmlspecialchars($message) ?>
