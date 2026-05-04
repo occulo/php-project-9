91140608cbe572e124f1ae6d3db64f15c95bfdd0
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-05-04 04:11:10 +0400

    fix(templates): handle urls without checks

--- templates/urls/index.php
+++ templates/urls/index.php
@@ -18,14 +18,14 @@
       <th scope="row"><?= $url['id'] ?></th>
       <td><a href="<?= $router->urlFor('url', ['url_id' => (string) $url['id']]) ?>" aria-label="<?= $url['name'] ?>"><?= $url['name'] ?></a></td>
       <td>
-        <?php if ($url['last_checked_at']) : ?>
+        <?php if (isset($url['last_checked_at'])) : ?>
             <?= date('d.m.Y H:i', strtotime($url['last_checked_at'])) ?>
         <?php else : ?>
           <span class="text-muted">---</span>
         <?php endif ?>
       </td>
       <td>
-        <?php if ($url['status_code']) : ?>
+        <?php if (isset($url['status_code'])) : ?>
             <?= $url['status_code'] ?>
         <?php else : ?>
           <span class="text-muted">---</span>
