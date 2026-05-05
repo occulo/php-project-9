3124bce8da77ec6f56094294e0d2941176e665d5
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-05-05 17:34:36 +0400

    refactor(helpers): replace global helpers with namespaced class

---
+++ src/Helpers/Str.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Hexlet\Code\Helpers;
+
+class Str
+{
+    public static function truncate(?string $str, int $length = 200): string
+    {
+        $str = $str ?? '';
+        if (mb_strlen($str) <= $length) {
+            return $str;
+        }
+        return sprintf("%s...", mb_substr($str, 0, $length));
+    }
+}
