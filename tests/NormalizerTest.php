bda3f0eeb99fa85ae872c4c1e41b99656e59dab7
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-05-05 18:57:36 +0400

    test(normalizer): use UrlNormalizer in normalization tests

--- tests/NormalizerTest.php
+++ tests/NormalizerTest.php
@@ -4,24 +4,24 @@ namespace Hexpet\Code\Tests;
 
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+use Hexlet\Code\UrlNormalizer;
 
 class NormalizerTest extends TestCase
 {
-    private const HOST = "example.com";
+    private const HOST = 'example.com';
 
     public static function urlProvider(): array
     {
         $schemes = ['http', 'https'];
-        $path = '/path';
-        $query = '?q=1';
+        $port = '8080';
+        $suffix = '/path?q=1#fragment';
 
         $data = [];
         foreach ($schemes as $scheme) {
             $baseUrl = sprintf("%s://%s", $scheme, self::HOST);
             $data["{$scheme} already normalized"] = [$baseUrl, $baseUrl];
-            $data["{$scheme} with path"] = ["{$baseUrl}{$path}", $baseUrl];
-            $data["{$scheme} with query"] = ["{$baseUrl}{$query}", $baseUrl];
-            $data["{$scheme} with path and query"] = ["{$baseUrl}{$path}{$query}", $baseUrl];
+            $data["{$scheme} with port"] = ["{$baseUrl}:{$port}", "{$baseUrl}:{$port}"];
+            $data["{$scheme} with suffix"] = ["{$baseUrl}{$suffix}", $baseUrl];
         }
         return $data;
     }
@@ -29,8 +29,7 @@ class NormalizerTest extends TestCase
     #[DataProvider('urlProvider')]
     public function testUrlNormalization(string $url, string $expected): void
     {
-        $parsedUrl = parse_url($url);
-        $normalizedUrl = sprintf("%s://%s", $parsedUrl['scheme'], $parsedUrl['host']);
+        $normalizedUrl = UrlNormalizer::normalize($url);
         $this->assertSame($expected, $normalizedUrl);
     }
 }
